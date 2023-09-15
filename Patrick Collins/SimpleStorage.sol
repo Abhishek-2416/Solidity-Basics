72234457c1bd54c0f037b696f08c023aa9cc29a6
Abhishek-1624 <abhishekalimchandani1624@gmail.com>
2023-09-15 22:25:14 +0530

    Patrick Collins

---
+++ Patrick Collins/SimpleStorage.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19; //This is our solidity version
+
+//The contract is something similar to a class what we use in any Object oriented language
+contract SimpleStorage{
+    
+}
+
+
+
+
+
+/*--------------------------------------------------------------------------NOTE------------------------------------------------------------------------
+                                                                      BITS vs BYTES
+    Bits are the smallest unit , both bits and bytes are used to understand the memory of a computer , everything is stored in terms of 0 or 1 in bits
+    1 byte = 8 bits
+    The general rule is we use bits for "interface speed" and bytes for "data storage"
+    
+
+-------------------------------------------------------------------------------------------------------------------------------------------------------*/
