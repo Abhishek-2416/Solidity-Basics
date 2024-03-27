7c82c2ad5393a711487094c22c054e96f32e74cb
Abhishek-1624 <abhishekalimchandani1624@gmail.com>
2024-03-27 14:10:44 +0530

    Basics

--- contracts/17_Encoding.sol
+++ contracts/17_Encoding.sol
@@ -5,6 +5,5 @@ contract Encoding {
     function combineStrings() public pure returns(string memory){
         //so here basically we are combining string using abi.encodePacked which is first taking both string, comvines it in its bytes object and then we convert it into the string type        
         return(string(abi.encodePacked("Hello ","World")));
-        
     }
 }
