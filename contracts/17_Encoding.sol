1a94c355347301f832b3991ca499102236d066c2
Abhishek-1624 <abhishekalimchandani1624@gmail.com>
2024-03-27 13:49:36 +0530

    Basics

---
+++ contracts/17_Encoding.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.20;
+
+contract Encoding {
+    function combineStrings() public pure returns(string memory){
+        //so here basically we are combining string using abi.encodePacked which is first taking both string, comvines it in its bytes object and then we convert it into the string type        
+        return(string(abi.encodePacked("Hello ","World")));
+        
+    }
+}
